6b91427d4740a9bd9fbd87ea1ba129beaafde9ab
Galymzhan <kozhayev@gmail.com>
2012-01-15 23:48:31 +0600

    fix unicode errors by treating input as UTF8

--- spec/interpreter_spec.rb
+++ spec/interpreter_spec.rb
@@ -13,6 +13,7 @@ describe Interpreter do
     mask = File.join(File.dirname(__FILE__), 'source', '**', '*.lol')
     Dir.glob(mask).each do |source_filename|
       lexer = Lexer.new(File.new(source_filename))
+      lexer.input.set_encoding(Encoding::UTF_8)
       parser = Parser.new(Tokenizer.new(lexer))
       @interpreter.output.string = ''
 
