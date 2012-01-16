a7a5876ab18c45040f96bd3141edee2a2829cb50
Galymzhan <kozhayev@gmail.com>
2012-01-16 18:47:34 +0600

    add version and let the layo binary work in interactive mode by default

--- lib/layo.rb
+++ lib/layo.rb
@@ -8,5 +8,10 @@ require_relative 'layo/tokenizer'
 require_relative 'layo/parser'
 require_relative 'layo/base_interpreter'
 require_relative 'layo/interpreter'
+require_relative 'layo/interactive_interpreter'
 require_relative 'layo/ast'
 require_relative 'layo/unicode'
+
+module Layo
+  VERSION = '1.1.0'
+end
